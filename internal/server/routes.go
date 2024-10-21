188ad55d6b0b07d50315cf11b4556a70e4ffe01c
Bastien <bastien.pouessel@epita.fr>
2024-10-20 21:36:05 -0700

    feat(server): add handlers for MethodNotAllowed and NotFound

--- internal/server/routes.go
+++ internal/server/routes.go
@@ -1,6 +1,7 @@
 package server
 
 import (
+	"fmt"
 	"log/slog"
 	"net/http"
 	"os"
@@ -10,6 +11,9 @@ import (
 )
 
 func addRoutes(router *httprouter.Router, logger *slog.Logger) http.Handler {
+	router.MethodNotAllowed = methodNotAllowedHandler(logger)
+	router.NotFound = notFoundHandler(logger)
+
 	router.Handler(http.MethodGet, "/v1/healthcheck", healthGETHandler())
 
 	standard := alice.New(
@@ -19,6 +23,20 @@ func addRoutes(router *httprouter.Router, logger *slog.Logger) http.Handler {
 	)
 
 	return standard.Then(router)
+}
+
+func notFoundHandler(logger *slog.Logger) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		msg := "the requested resource could not be found"
+		errorResponse(logger, w, r, http.StatusNotFound, msg)
+	})
+}
+
+func methodNotAllowedHandler(logger *slog.Logger) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		msg := fmt.Sprintf("the %s method is not supported for this resource", r.Method)
+		errorResponse(logger, w, r, http.StatusMethodNotAllowed, msg)
+	})
 }
 
 func healthGETHandler() http.Handler {
