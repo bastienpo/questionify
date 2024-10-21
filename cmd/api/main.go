4ec561eecbe08635468f58e7fbd2e9d07ded4d59
Bastien <bastien.pouessel@epita.fr>
2024-10-20 20:32:24 -0700

    refactor(main): gracefully shutdown the server
    - Split the code in a gracefulShutdown go routine and move the run
      command

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,33 +1,59 @@
 package main
 
 import (
-	"errors"
+	"context"
+	"fmt"
+	"io"
 	"log/slog"
 	"net/http"
 	"os"
+	"os/signal"
 	"questionify/internal/server"
+	"syscall"
+	"time"
 )
 
-func main() {
-	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+func gracefulShutdown(ctx context.Context, srv *http.Server, done chan struct{}, logger *slog.Logger) {
+	ctx, cancel := signal.NotifyContext(ctx, syscall.SIGINT, syscall.SIGTERM)
+	defer cancel()
 
-	shutdownError := make(chan error)
-	srv := server.NewServer(logger, shutdownError)
+	// Wait for the signal
+	<-ctx.Done()
+
+	logger.Info("Shutting down server")
+
+	ctx, cancel = context.WithTimeout(ctx, 20*time.Second)
+	defer cancel()
+
+	if err := srv.Shutdown(ctx); err != nil {
+		logger.Error("server shutdown error", "error", err)
+	}
+
+	logger.Info("Server shutdown completed")
+
+	close(done)
+}
+
+func run(ctx context.Context, w io.Writer, args []string) error {
+	logger := slog.New(slog.NewTextHandler(w, nil))
+	srv := server.NewServer(logger)
+
+	done := make(chan struct{})
+	go gracefulShutdown(ctx, srv, done, logger)
 
 	logger.Info("Starting server", "port", srv.Addr)
 
-	err := srv.ListenAndServe()
-	if !errors.Is(err, http.ErrServerClosed) {
-		logger.Error("server error", "error", err)
-		os.Exit(1)
+	if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		return fmt.Errorf("http server error: %s", err)
 	}
 
-	err = <-shutdownError
-	if err != nil {
-		logger.Error("server error", "error", err)
+	<-done
+	return nil
+}
+
+func main() {
+	if err := run(context.Background(), os.Stdout, os.Args); err != nil {
+		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
-
-	logger.Info("server shutdown")
-	os.Exit(0)
 }
--- internal/server/server.go
+++ internal/server/server.go
@@ -1,21 +1,19 @@
 package server
 
 import (
-	"context"
 	"fmt"
 	"log/slog"
 	"net/http"
 	"os"
-	"os/signal"
 	"strconv"
-	"syscall"
 	"time"
 
 	"github.com/julienschmidt/httprouter"
 )
 
-func NewServer(logger *slog.Logger, shutdownError chan error) *http.Server {
+func NewServer(logger *slog.Logger) *http.Server {
 	port, err := strconv.Atoi(os.Getenv("PORT"))
+
 	if err != nil {
 		logger.Error("failed to parse port", "error", err)
 		return nil
@@ -29,20 +27,8 @@ func NewServer(logger *slog.Logger, shutdownError chan error) *http.Server {
 		IdleTimeout:  time.Minute,
 		ReadTimeout:  10 * time.Second,
 		WriteTimeout: 30 * time.Second,
+		ErrorLog:     slog.NewLogLogger(logger.Handler(), slog.LevelError),
 	}
-
-	go func() {
-		quit := make(chan os.Signal, 1)
-		signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
-		s := <-quit
-
-		logger.Info("Shutting down server", "signal", s.String())
-
-		ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
-		defer cancel()
-
-		shutdownError <- srv.Shutdown(ctx)
-	}()
 
 	return srv
 }
