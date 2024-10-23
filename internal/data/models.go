dfb451ed94ffa054d02dfb1b8f717ac009f1d458
Bastien <bastien.pouessel@epita.fr>
2024-10-23 16:58:24 -0700

    feat(token): add method to generate a new token
    - Insert the token in the database and generate the hash (sha256) of the
      token
    - Add a function to delete all the token from a specific user

--- internal/data/models.go
+++ internal/data/models.go
@@ -5,11 +5,13 @@ import "database/sql"
 type ModelStore struct {
 	Users         UserModel
 	Conversations ConversationModel
+	Tokens        TokenModel
 }
 
 func NewModelStore(db *sql.DB) *ModelStore {
 	return &ModelStore{
 		Users:         UserModel{DB: db},
 		Conversations: ConversationModel{DB: db},
+		Tokens:        TokenModel{DB: db},
 	}
 }
